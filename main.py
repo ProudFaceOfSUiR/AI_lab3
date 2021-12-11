78854c1632d4afdf5b7c0b207ec3d1880db7c417
Anatoly Novikov <anatoly_novikov@Anatolys-MacBook-Pro.local>
2021-12-11 08:43:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,171 @@
+import numpy as np # linear algebra
+import os
+import torch
+import torchvision
+from torchvision.datasets.utils import download_url
+from torch.utils.data import random_split
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+from torch.utils.data.dataloader import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+
+
+    random.seed(0)
+    torch.manual_seed(0)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(device)
+
+    FOLDERNAME = 'flowers'
+
+    assert FOLDERNAME is not None, "[!] Enter the foldername."
+
+
+    prepare_imgs = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize((224, 224)), #приводим картинки к одному размеру
+            torchvision.transforms.ToTensor(), # упаковывем их в тензор
+            torchvision.transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225] # нормализуем картинки по каналам
+            ),
+        ]
+    )
+    # задаем датасет. Лейблы - имена папок:
+    dataset = ImageFolder('flowers', transform=prepare_imgs)
+
+    dataset.imgs[2]
+
+    class ValueMeter(object):
+      """
+      Вспомогательный класс, чтобы отслеживать loss и метрику
+      """
+      def __init__(self):
+          self.sum = 0
+          self.total = 0
+
+      def add(self, value, n):
+          self.sum += value*n
+          self.total += n
+
+      def value(self):
+          return self.sum/self.total
+
+    def log(mode, epoch, loss_meter, accuracy_meter, best_perf=None):
+      """
+      Вспомогательная функция, чтобы
+      """
+      print(
+          f"[{mode}] Epoch: {epoch:0.2f}. "
+          f"Loss: {loss_meter.value():.2f}. "
+          f"Accuracy: {100*accuracy_meter.value():.2f}% ", end="\n")
+
+      if best_perf:
+          print(f"[best: {best_perf:0.2f}]%", end="")
+
+
+
+    model = nn.Sequential(
+                nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2), # output: 64 x 16 x 16
+
+                nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2), # output: 128 x 8 x 8
+
+                nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2), # output: 256 x 4 x 4
+
+                nn.Flatten(),
+                nn.Linear(256*28*28, 1024),
+                nn.ReLU(),
+                nn.Linear(1024, 512),
+                nn.ReLU(),
+                nn.Linear(512, 5))
+    model.to(device) # отправляем модель на девайс (GPU)
+
+    batch_size = 32 # размер батча
+    optimizer = torch.optim.Adam(params = model.parameters()) # алгоритм оптимизации
+    lr = 0.001 # learning rate
+
+    train_set, val_set = torch.utils.data.random_split(dataset, [len(dataset)-1000, 1000])
+    print('Размер обучающего и валидационного датасета: ', len(train_set), len(val_set))
+    loaders = {'training': DataLoader(train_set, batch_size, pin_memory=True,num_workers=2, shuffle=True),
+               'validation':DataLoader(val_set, batch_size, pin_memory=True,num_workers=2, shuffle=False)}
+
+    def accuracy(outputs, labels):
+        _, preds = torch.max(outputs, dim=1)
+        return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+    def trainval(model, loaders, optimizer, epochs=10):
+        """
+        model: модель, которую собираемся обучать
+        loaders: dict с dataloader'ами для обучения и валидации
+        optimizer: оптимизатор
+        epochs: число обучающих эпох (сколько раз пройдемся по всему датасету)
+        """
+        loss_meter = {'training': ValueMeter(), 'validation': ValueMeter()}
+        accuracy_meter = {'training': ValueMeter(), 'validation': ValueMeter()}
+
+        loss_track = {'training': [], 'validation': []}
+        accuracy_track = {'training': [], 'validation': []}
+
+        for epoch in range(epochs): # итерации по эпохам
+            for mode in ['training', 'validation']: # обучение - валидация
+                # считаем градиаент только при обучении:
+                with torch.set_grad_enabled(mode == 'training'):
+                    # в зависимоти от фазы переводим модель в нужный ружим:
+                    model.train() if mode == 'training' else model.eval()
+                    for imgs, labels in tqdm(loaders[mode]):
+                        imgs = imgs.to(device) # отправляем тензор на GPU
+                        labels = labels.to(device)
+                        bs = labels.shape[0]  # размер батча (отличается для последнего батча в лоадере)
+
+                        preds = model(imgs) # forward pass - прогоняем тензор с картинками через модель
+                        loss = F.cross_entropy(preds, labels) # считаем функцию потерь
+                        acc = accuracy(preds, labels) # считаем метрику
+
+                        # храним loss и accuracy для батча
+                        loss_meter[mode].add(loss.item(), bs)
+                        accuracy_meter[mode].add(acc, bs)
+
+                        # если мы в фазе обучения
+                        if mode == 'training':
+                            optimizer.zero_grad() # обнуляем прошлый градиент
+                            loss.backward() # делаем backward pass (считаем градиент)
+                            optimizer.step() # обновляем веса
+                # в конце фазы выводим значения loss и accuracy
+                log(mode, epoch, loss_meter[mode], accuracy_meter[mode])
+
+                # сохраняем результаты по всем эпохам
+                loss_track[mode].append(loss_meter[mode].value())
+                accuracy_track[mode].append(accuracy_meter[mode].value())
+        return loss_track, accuracy_track
+
+    loss_track, accuracy_track = trainval(model, loaders, optimizer, epochs=10)
+
+    from matplotlib import pyplot as plt
+
+    plt.plot(accuracy_track['training'], label='training')
+    plt.plot(accuracy_track['validation'], label='validation')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
